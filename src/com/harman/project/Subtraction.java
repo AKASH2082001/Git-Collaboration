965b37d3cb6adfbfd28d377af25885e319b58fa7
Leela05 <leelarenu05@gmail.com>
2022-03-19 23:06:41 +0530

    Sub

--- src/com/harman/project/Subtraction.java
+++ src/com/harman/project/Subtraction.java
@@ -1,5 +1,12 @@
 package com.harman.project;
 
 public class Subtraction {
+    public static void main(String[] args) {
+        int number1,number2,number3;
+        number1=4;
+        number2=5;
+        number3=number1+number2;
+        System.out.println(number3);
+    }
 
 }
